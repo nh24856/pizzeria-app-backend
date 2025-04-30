f57dbf39fdac8eb1350796aa6b678d47c2658986
nh24856 <heritiernkinzingabo@gmail.com>
2025-04-30 10:07:18 +0200

    changes made on cloudinary

--- src/database/migrations/config/cloudinary.js
+++ src/database/migrations/config/cloudinary.js
@@ -1,10 +1,12 @@
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
+//from .env
 cloudinary.config({
-  cloud_name: "dhoew2axt",
-  api_key: "729177997934669",
-  api_secret: "dPht94cTuQrz1bG8P9FuwKFzEcQ",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  
 });
 
 module.exports = cloudinary;
