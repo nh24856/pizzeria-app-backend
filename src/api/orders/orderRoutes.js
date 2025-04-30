872afc2d214916ed4613c7af0785cfd2525476a9
nh24856 <heritiernkinzingabo@gmail.com>
2025-04-30 14:03:12 +0200

    modified existing function and their routes

--- src/api/orders/orderRoutes.js
+++ src/api/orders/orderRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, getOrders, getOrderById, updateOrderStatus, getOrderCount, getSelfOrders, getMyOrders, getMyOrderCount, getPendingOrderCount} = require('./orderController');
+const { createOrder, getOrders, orderCancel, getOrderById, updateOrderStatus, getOrderCount, getselfOrders, getSelfOrderscount, getMyOrders, getMyOrderCount, getPendingOrderCount} = require('./orderController');
 const { authenticate, authorize, } = require('../users/userMiddlewares');
 const { canUpdateOrderStatus } = require('./orderMiddleware');
 
 // Public Routes
 router.post('/register', authenticate, authorize('customer'), createOrder);
-router.put('/update-order-status/:id/status', authenticate, canUpdateOrderStatus, updateOrderStatus);
-router.put('/update-order', authenticate, authorize('customer'));
+router.put('/update-order-status/:id/status', authenticate, authorize('customer', 'Seller'), canUpdateOrderStatus, updateOrderStatus);
+router.put('/update-order', authenticate, authorize('customer'), orderCancel);
 router.get('/get-order/:id', authenticate, authorize('Seller'), getOrderById);
 router.get('/get-all-orders', getOrders);
 router.get('/get-order-count', getOrderCount);
-router.get('/get-self-orders/:userId',authenticate, authorize('customer'), getSelfOrders);
+router.get('/get-self-orders/:userId', authenticate, authorize('customer'), getselfOrders);
+router.get('/get-self-orders-count/:userId',authenticate, authorize('customer'), getSelfOrderscount);
 router.get('/get-my-orders/:userId', getMyOrders);
 router.get('/get-my-order-count/:userId', getMyOrderCount);
 router.get('/get-pending-order-count/:userId', getPendingOrderCount);
